a7cc4a68444dd023f69282edfdd210ab234207ec
sarahdippolito <dippolitos@usf.edu>
2025-01-21 14:10:23 -0500

    Updated code

--- assignmentOne.R
+++ assignmentOne.R
@@ -1,4 +1,7 @@
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
 myMean(assignment2)
-change <- 9
+Simpletest <- c(2,2)
+myMean(Simpletest)
+test <- c(1,6,89,20,12,13,8,1:6)
+myMean(test)
