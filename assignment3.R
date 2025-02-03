ec1809a9eda8881e5348e2f37e7d6fde9b6a1fa2
sarahdippolito <dippolitos@usf.edu>
2025-02-03 08:12:59 -0500

    Commmit With Answers

--- assignment3.R
+++ assignment3.R
@@ -1,14 +1,25 @@
+Name = c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC_political = c(4, 62, 51, 21, 2, 14, 15)
+NCB_political = c(12, 75, 43, 19, 1, 21, 19)
+results = cbind(Name, ABC_political, NCB_political)
+results
+results.df = data.frame(Name, ABC_political, NCB_political)
+results.df
+r = rowMeans(results.df[,2:3])
+r.df = data.frame(Name, r)
+mean(as.matrix(results.df[,2:3]))
+
+#
+
 Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
 Name
-
 ABC_result  <- c(4, 62, 51, 21, 2, 14, 15)
 ABC_result
-
 CBS_result  <- c(12, 75, 43, 19, 1, 21, 19) 
 CBS_result
-
 election_poles <- data.frame(Name, ABC_result, CBS_result)
 election_poles 
-
 colnames(election_poles) <- c("Candidate", "ABC Pole", "CBC Pole")
 election_poles
+
+
