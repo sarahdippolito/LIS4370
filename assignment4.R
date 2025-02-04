c579989f5195c5ac3d11063e4340879686dc0ade
sarahdippolito <dippolitos@usf.edu>
2025-02-03 19:05:36 -0500

    commit

--- assignment4.R
+++ assignment4.R
@@ -23,3 +23,5 @@ par(mfrow=c(1,1))
 first1 <- c("Bad", "Bad", "Bad", "Bad", "Good", "Good", "Good", "Good", NA, "Bad")
 first1 <- table(first1)
 barplot(first1, main="Barplot of GD Assesment", col=c("red", "green"))
+
+boxplot(bloodp ~ first, data=medical.df, col=c("green", "red"), main="BoxPlot with Blood Pressure and GD Assesment", xlab="GD's Assesment", ylab="Blood Pressure")
