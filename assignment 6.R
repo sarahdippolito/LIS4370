47a53b6b616cba7d4d265a02b197760e10e0c5f3
sarahdippolito <dippolitos@usf.edu>
2025-02-18 08:25:03 -0500

    assignment 6

---
+++ assignment 6.R
@@ -0,0 +1,12 @@
+A = matrix(c(2,0,1,3), ncol=2)
+A
+B = matrix(c(5,2,4,-1), ncol=2)
+B
+A + B
+A - B
+diag(c(4,1,2,3), 4)
+M <- matrix(0, 5,5)
+M[1, ] <- c(rep(1, 5))
+M[,1] <- c(rep(2,5))
+diag(M) <- 3
+M
